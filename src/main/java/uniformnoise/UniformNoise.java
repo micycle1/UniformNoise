50fc730687f62b790feea9083d08bb6fb3721d9b
Michael Carleton <micycle1@hotmail.co.uk>
2021-06-18 18:49:40 +0100

    Formalise

--- src/main/java/uniformnoise/UniformNoise.java
+++ src/main/java/uniformnoise/UniformNoise.java
@@ -12,26 +12,41 @@ public class UniformNoise {
 
 	private static int seed = ThreadLocalRandom.current().nextInt();
 
+	/**
+	 * Sets the seed used by all static noise methods.
+	 */
 	public static void setSeed(final int seed) {
 		UniformNoise.seed = seed;
 	}
 
 	/**
-	 * Single-octave 2D uniform perlin noise.
+	 * Generates single-octave 2D uniform perlin noise for the coordinates given.
 	 * 
 	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
 	 */
 	public static float uniformNoise(float x, float y) {
-		return G2D(PerlinNoiseLite.SinglePerlin(seed, x, y));
+		return cdf2D(PerlinNoiseLite.SinglePerlin(seed, x, y));
 	}
 
 	/**
-	 * Single-octave 3D uniform perlin noise.
+	 * Single-octave 3D uniform perlin noise for the coordinates given.
+	 * 
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
 	 */
 	public static float uniformNoise(float x, float y, float z) {
-		return G3D(PerlinNoiseLite.SinglePerlin(seed, x, y, z));
+		return cdf3D(PerlinNoiseLite.SinglePerlin(seed, x, y, z));
 	}
 
+	/**
+	 * Generates multi-octave 2D uniform perlin noise for the coordinates given.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param octaves     the number of levels of detail the perlin noise has
+	 * @param persistence determines how quickly the amplitudes fall for each
+	 *                    successive octave
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
+	 */
 	public static float uniformNoise(float x, float y, int octaves, float persistence) {
 		float noise = 0;
 
@@ -40,20 +55,21 @@ public class UniformNoise {
 		for (int i = 0; i < octaves; i++) {
 			noise += PerlinNoiseLite.SinglePerlin(seed, x * frequency, y * frequency) * amplitude;
 			amplitude *= persistence;
-			frequency *= 2;
+			frequency *= 2; // lacunarity
 		}
-		return H2D(noise);
+		return cdfOctaves2D(noise);
 	}
 
 	/**
-	 * OctaveNoise
+	 * Generates multi-octave 3D uniform perlin noise for the coordinates given.
 	 * 
 	 * @param x
 	 * @param y
 	 * @param z
-	 * @param octaves
-	 * @param persistence
-	 * @return
+	 * @param octaves     the number of levels of detail the perlin noise has
+	 * @param persistence determines how quickly the amplitudes fall for each
+	 *                    successive octave
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
 	 */
 	public static float uniformNoise(float x, float y, float z, int octaves, float persistence) {
 		float noise = 0;
@@ -63,13 +79,22 @@ public class UniformNoise {
 		for (int i = 0; i < octaves; i++) {
 			noise += PerlinNoiseLite.SinglePerlin(seed, x * frequency, y * frequency, z * frequency) * amplitude;
 			amplitude *= persistence;
-			frequency *= 2;
+			frequency *= 2; // lacunarity
 		}
-		return H3D(noise);
+		return cdfOctaves3D(noise);
 	}
 
-	private static float G2D(float x) {
-		if (x < -0.73) {
+	/**
+	 * Fifth-degree polynomial approximation of a continuous distribution function
+	 * for 2D perlin noise.
+	 * <p>
+	 * This approximation is derived from a set of 2D single-octave noise values.
+	 * 
+	 * @param x raw (gaussian-like) perlin noise between [-1...1]
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
+	 */
+	private static float cdf2D(float x) {
+		if (x < -0.729) {
 			return 0;
 		}
 		if (x > 0.751) {
@@ -79,12 +104,15 @@ public class UniformNoise {
 	}
 
 	/**
-	 * Single-octave 3D noise
+	 * Fifth-degree polynomial approximation of a continuous distribution function
+	 * for 3D perlin noise.
+	 * <p>
+	 * This approximation is derived from a set of 3D single-octave noise values.
 	 * 
-	 * @param x
-	 * @return
+	 * @param x raw (gaussian-like) perlin noise between [-1...1]
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
 	 */
-	private static float G3D(float x) {
+	private static float cdf3D(float x) {
 		if (x < -0.662) {
 			return 0;
 		}
@@ -95,34 +123,17 @@ public class UniformNoise {
 	}
 
 	/**
-	 * Optimised for 3d perlin noise with 4 octaves, 0.5 falloff between -0.8 and
-	 * 0.8. * Fifth-degree polynomial approximation of continuous distribution
-	 * function for regular perlin noise. The purpose of this method is to flatten
-	 * (since Perlin noise distributions follow a normal/gaussian distribution).
+	 * Fifth-degree polynomial approximation of a continuous distribution function
+	 * for 2D perlin noise.
+	 * <p>
+	 * This approximation is derived from a set of 2D noise values having octaves=4
+	 * and persistence=0.5; the output may be increasingly non-uniform with input
+	 * noise generated using a different number of octaves or persistence.
 	 * 
 	 * @param x raw (gaussian-like) perlin noise between [-1...1]
 	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
 	 */
-	private static float H3D(float x) {
-		if (x < -0.796) {
-			return 0;
-		}
-		if (x > 0.793) {
-			return 1;
-		}
-		/*
-		 * Approximation on interval [ -0.8, 0.8 ] with a polynomial of degree 5.
-		 */
-		return ((((9.8832232e-1f * x + 2.1932919e-3f) * x - 1.6058296f) * x - 1.3540642e-3f) * x + 1.2490637f) * x + 5.0019288e-1f;
-	}
-
-	/**
-	 * Optimised for 2d perlin noise with 4 octaves, 0.5 persistence.
-	 * 
-	 * @param x
-	 * @return
-	 */
-	private static float H2D(float x) {
+	private static float cdfOctaves2D(float x) {
 		if (x < -0.897) {
 			return 0;
 		}
@@ -133,6 +144,30 @@ public class UniformNoise {
 		 * Approximation on interval [ -0.9, 0.9 ] with a polynomial of degree 5.
 		 */
 		return ((((5.5801775e-1f * x + 3.4876526e-3f) * x - 1.102035f) * x - 2.1366362e-3f) * x + 1.083566f) * x + 5.0009794e-1f;
+	}
+
+	/**
+	 * Fifth-degree polynomial approximation of a continuous distribution function
+	 * for 3D perlin noise.
+	 * <p>
+	 * This approximation is derived from a set of 3D noise values having octaves=4
+	 * and persistence=0.5; the output may be increasingly non-uniform with input
+	 * noise generated using a different number of octaves or persistence.
+	 * 
+	 * @param x raw (gaussian-like) 3D perlin noise between [-1...1]
+	 * @return approximately uniformly distributed noise value between [ 0, 1 ]
+	 */
+	private static float cdfOctaves3D(float x) {
+		if (x < -0.796) {
+			return 0;
+		}
+		if (x > 0.793) {
+			return 1;
+		}
+		/*
+		 * Approximation on interval [ -0.8, 0.8 ] with a polynomial of degree 5.
+		 */
+		return ((((9.8832232e-1f * x + 2.1932919e-3f) * x - 1.6058296f) * x - 1.3540642e-3f) * x + 1.2490637f) * x + 5.0019288e-1f;
 	}
 
 }
